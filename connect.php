28ff2d6e20c528ceb021cf656b41848945d2f83c
Tiến-Anh-1123 <tienanh17022002@gmail.com>
2023-09-16 21:19:13 +0700

    tienanh

--- connect.php
+++ connect.php
@@ -1,13 +1,19 @@
 <?php 
-    //Kết nối
-    $servername= "localhost";
-    $username="root";
-    $password="";
-    $dbname="banmu";
-    $conn=mysqli_connect($servername,$username,$password,$dbname);
+    // //Kết nối
+    // $servername= "localhost";
+    // $username="root";
+    // $password="";
+    // $dbname="banmu";
+    // $conn=mysqli_connect($servername,$username,$password,$dbname);
 
-    //kiểm tra kết nối
-    if(!$conn){
-        die("Connection failed: ".mysqli_connect_error());
-    }else;
+    // //kiểm tra kết nối
+    // if(!$conn){
+    //     die("Connection failed: ".mysqli_connect_error());
+    // }else;
+
+
+    $conn = mysqli_init();
+    mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+    mysqli_real_connect($conn, "munonchat.mysql.database.azure.com", "tienanhdz", "phuc17@@", "banmu", 3306, MYSQLI_CLIENT_SSL);
  ?>
+ 
